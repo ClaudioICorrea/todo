501d18ae1c38e5390c2963bd39d21dc080bb60ab
Claudio Correa <clcorrea@udec.cl>
2023-04-18 17:09:34 -0400

    so far so good

--- todo/todo.py
+++ todo/todo.py
@@ -1,3 +1,5 @@
+
+from datetime import datetime
 from flask import(
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -11,16 +13,39 @@ bp = Blueprint('todo',__name__)
 def index():
     db, c = get_db()
     c.execute(
-        'SELECT t.id, t.description, u.username, t.completed, t.created_at t FROM todo t JOIN user u on t.created_by = u.id ORDER BY created_at DESC'
+        'SELECT t.id, t.created_at, t.description, u.username, t.completed  t FROM todo t JOIN user u on t.created_by = u.id ORDER BY created_at desc'
     )
-    todos = c.fetchall()
-    return render_template('todo/index.html', todos=todos)
-@bp.route('/create',methods=['GET','POST'])
+    luke = c.fetchall()
+    return render_template('todo/index.html', todos=luke)
+@bp.route('/_create',methods=['GET','POST'])
 @login_required
-def create():
-    return ''
+def _create():
+    if request.method == 'POST':
+        description = request.form['description']
+        error = None
+        if not description:
+            error = 'Descripción es requerida'
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'insert into todo (description, completed, created_by)'
+                ' values (%s,%s,%s)',
+                (description, False, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
 
-@bp.route('/update', methods=['GET','POST'])
+    return render_template('todo/_create.html')
+
+@bp.route('/<int:id>/_update', methods=['GET','POST'])
 @login_required
-def update():
+def _update(id):
+    return render_template('todo/_update.html', todo=todo)
+
+
+@bp.route('/<int:id>/delete', methods=['POST'])
+@login_required
+def delete():
     return ''
